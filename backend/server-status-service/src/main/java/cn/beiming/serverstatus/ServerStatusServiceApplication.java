2bc251f5881b19bf6d7e4fa941cf96e9f5c2ad99
花蝉夜影 <1578086557@qq.com>
2026-05-22 17:48:46 +0800

    test: add server status contract tests

---
+++ backend/server-status-service/src/main/java/cn/beiming/serverstatus/ServerStatusServiceApplication.java
@@ -0,0 +1,11 @@
+package cn.beiming.serverstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStatusServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStatusServiceApplication.class, args);
+    }
+}
